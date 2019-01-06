a237928f78ef954201a6036d2c56a6aacd3557f7
Scott N Fitz <doctorwidget@gmail.com>
2019-01-06 11:46:01 -0800

    solutions for 01_equalities

--- src/koans/01_equalities.clj
+++ src/koans/01_equalities.clj
@@ -2,38 +2,41 @@
   (:require [koan-engine.core :refer :all]))
 
 (meditations
-  "We shall contemplate truth by testing reality, via equality"
-  (= __ true)
+ "We shall contemplate truth by testing reality, via equality"
+ (= true true) ; sanity test for reality
 
-  "To understand reality, we must compare our expectations against reality"
-  (= __ (+ 1 1))
+ "To understand reality, we must compare our expectations against reality"
+ (= 2 (+ 1 1)) ; expressions are evalated in inside-out order
 
-  "You can test equality of many things"
-  (= (+ 3 4) 7 (+ 2 __))
+ "You can test equality of many things"
+ (= (+ 3 4) 7 (+ 2 5)) ; '=' can take any number of arguments. (NB: many koans rely on this)
 
-  "Some things may appear different, but be the same"
-  (= __ (= 2 2/1))
+ "Some things may appear different, but be the same"
+ (= true (= 2 2/1)) ; integers and ratios can be compared with =
+ 
+ "You cannot generally float to heavens of integers"
+ (= false (= 2 2.0)) ; integers and floats CANNOT be compared with =
+ 
+ "But a looser equality is also possible"
+ (= true (== 2.0 2)) ; use '==' to get loose numeric equality check
+ 
+ "Something is not equal to nothing"
+ (= true (not (= 1 nil))) ; 1 is not nothing
+ 
+ "Zero is something"
+ (= true (not (= 0 nil))) ; more interestingly, zero is not nothing either
+ 
+ "Strings, and keywords, and symbols: oh my!"
+ (= false (= "hello" :hello 'hello)) ; Clojure almost never autocasts 
 
-  "You cannot generally float to heavens of integers"
-  (= __ (= 2 2.0))
+ "Make a keyword with your keyboard"
+ (= :hello (keyword "hello")) ; but you can easily do your own casting
 
-  "But a looser equality is also possible"
-  (= __ (== 2.0 2))
+ "Symbolism is all around us"
+ (= 'hello (symbol "hello")) ;; make a symbol from a string
 
-  "Something is not equal to nothing"
-  (= __ (not (= 1 nil)))
+ "What could be equivalent to nothing?"
+ (= nil nil) ; nil is only equal to itself
 
-  "Strings, and keywords, and symbols: oh my!"
-  (= __ (= "hello" :hello 'hello))
-
-  "Make a keyword with your keyboard"
-  (= :hello (keyword __))
-
-  "Symbolism is all around us"
-  (= 'hello (symbol __))
-
-  "What could be equivalent to nothing?"
-  (= __ nil)
-
-  "When things cannot be equal, they must be different"
-  (not= :fill-in-the-blank __))
+ "When things cannot be equal, they must be different"
+ (not= :fill-in-the-blank 1)) ; almost anything works here
