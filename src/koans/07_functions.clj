43db26782ac62489c86e608e7ac0f15465d4d619
Scott N Fitz <doctorwidget@gmail.com>
2019-01-06 16:10:32 -0800

    solutions for 07_functions

--- src/koans/07_functions.clj
+++ src/koans/07_functions.clj
@@ -8,33 +8,33 @@
 
 (meditations
   "Calling a function is like giving it a hug with parentheses"
-  (= __ (square 9))
+  (= 81 (square 9)) ; lisp order is best order
 
   "Functions are usually defined before they are used"
-  (= __ (multiply-by-ten 2))
+  (= 20 (multiply-by-ten 2)) ; dash-case is idiomatic clojure
 
   "But they can also be defined inline"
-  (= __ ((fn [n] (* 5 n)) 2))
+  (= 10 ((fn [n] (* 5 n)) 2)) ; fn is fundamental. One of the original special forms! 
 
   "Or using an even shorter syntax"
-  (= __ (#(* 15 %) 4))
+  (= 60 (#(* 15 %) 4)) ; whereas this #-based reader macro is a late-comer
 
   "Even anonymous functions may take multiple arguments"
-  (= __ (#(+ %1 %2 %3) 4 5 6))
+  (= 15 (#(+ %1 %2 %3) 4 5 6)) ; NB: 1-based counting, not 0-based!
 
   "Arguments can also be skipped"
-  (= __ (#(str "AA" %2) "bb" "CC"))
+  (= "AACC" (#(str "AA" %2) "bb" "CC")) ; 'skipping' here means *ignoring*; it is an error not to *provide* all arguments
 
   "One function can beget another"
-  (= 9 (((fn [] ___)) 4 5))
+  (= 9 (((fn [] +)) 4 5)) ; you don't need to quote the '+' here... it's perfectly safe in the non-function position
 
   "Functions can also take other functions as input"
   (= 20 ((fn [f] (f 4 5))
-           ___))
+           *)) ; same here - providing the '*' function as a first-class function is perfectly kosher
 
   "Higher-order functions take function arguments"
-  (= 25 (___
+  (= 25 ((fn [f] (f 5))  ; so this fn expects another fn... it's functions all the way down!
           (fn [n] (* n n))))
 
   "But they are often better written using the names of functions"
-  (= 25 (___ square)))
+  (= 25 ((fn [f] (f 5)) square))) ; it seems to me that too many anonymous functions is an antipattern
