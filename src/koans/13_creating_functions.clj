376e7219fa8745a1b5558055046435a1a86e2d44
Scott N Fitz <doctorwidget@gmail.com>
2019-01-06 17:10:34 -0800

    solutions for 13_creating_functions

--- src/koans/13_creating_functions.clj
+++ src/koans/13_creating_functions.clj
@@ -4,32 +4,39 @@
 (defn square [x] (* x x))
 
 (meditations
-  "One may know what they seek by knowing what they do not seek"
-  (= [__ __ __] (let [not-a-symbol? (complement symbol?)]
-                  (map not-a-symbol? [:a 'b "c"])))
+ "One may know what they seek by knowing what they do not seek"
+ (= [true false true] (let [not-a-symbol? (complement symbol?)]
+                        (map not-a-symbol? [:a 'b "c"])))
+ ; (complement) is a HOF that doubles the value of the standard library comparators
+ 
+ "Praise and 'complement' may help you separate the wheat from the chaff"
+ (= [:wheat "wheat" 'wheat]
+    (let [not-nil? (complement nil?)]
+      (filter not-nil? [nil :wheat nil "wheat" nil 'wheat nil])))
+; it can make things _much_ clearer to define a local helper with (complement)
+ 
+ "Partial functions allow procrastination"
+ (= 20 (let [multiply-by-5 (partial * 5)]
+         (multiply-by-5 4)))
+ ; pin any number arguments to the front of a fn with (partial)
+ 
+ "Don't forget: first things first"
+ (= [:a :b :c :d]
+    (let [ab-adder (partial concat [:a :b])]
+      (ab-adder [:c :d])))
+ ; a local (partial) is often the more idiomatic choice in Clojure,  vs a top-level named helper
+ 
+ "Functions can join forces as one 'composed' function"
+ (= 25 (let [inc-and-square (comp square inc)]
+         (inc-and-square 4)))
+ ; (comp) lets you chain functions; they will be applied from RIGHT to LEFT (opposite of reading order)
+ 
+ "Have a go on a double dec-er"
+ (= 8 (let [double-dec (comp dec dec)]
+         (double-dec 10)))
+ ; (though the order of application doesn't matter if you're just repeating the same fn)
 
-  "Praise and 'complement' may help you separate the wheat from the chaff"
-  (= [:wheat "wheat" 'wheat]
-       (let [not-nil? ___]
-         (filter not-nil? [nil :wheat nil "wheat" nil 'wheat nil])))
-
-  "Partial functions allow procrastination"
-  (= 20 (let [multiply-by-5 (partial * 5)]
-          (___ __)))
-
-  "Don't forget: first things first"
-  (= [__ __ __ __]
-       (let [ab-adder (partial concat [:a :b])]
-         (ab-adder [__ __])))
-
-  "Functions can join forces as one 'composed' function"
-  (= 25 (let [inc-and-square (comp square inc)]
-          (inc-and-square __)))
-
-  "Have a go on a double dec-er"
-  (= __ (let [double-dec (comp dec dec)]
-          (double-dec 10)))
-
-  "Be careful about the order in which you mix your functions"
-  (= 99 (let [square-and-dec ___]
-          (square-and-dec 10))))
+ "Be careful about the order in which you mix your functions"
+ (= 99 (let [square-and-dec (comp dec square)]
+         (square-and-dec 10))))
+; again, fn application is RIGHT to LEFT in a (comp) HOF 
