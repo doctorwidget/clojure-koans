bd1cb77d3e5f77ac2f9710ff3302acb2da50f4f5
Scott N Fitz <doctorwidget@gmail.com>
2019-01-06 16:55:37 -0800

    solutions for 11_lazy_sequences

--- src/koans/11_lazy_sequences.clj
+++ src/koans/11_lazy_sequences.clj
@@ -2,27 +2,32 @@
   (:require [koan-engine.core :refer :all]))
 
 (meditations
-  "There are many ways to generate a sequence"
-  (= __ (range 1 5))
+ "There are many ways to generate a sequence"
+ (= '(1 2 3 4) (range 1 5)) ; arity-2: first-included and first-excluded indices
+ 
+ "The range starts at the beginning by default"
+ (= '(0 1 2 3 4) (range 5)) ; arity-1: first-included index is 0 by default; %1 is first-excluded
+ 
+ "Only take what you need when the sequence is large"
+ (= [0 1 2 3 4 5 6 7 8 9]
+    (take 10 (range 100))) ; think of (range 100) as instantiating a __generator__ instance
+ 
+ "Or limit results by dropping what you don't need"
+ (= [95 96 97 98 99]
+    (drop 95 (range 100))) ; all clojure lazy sequences are surely generators under the hood
+ 
+ "Iteration provides an infinite lazy sequence"
+ (= [1 2 4 8 16 32 64 128] (take 8 (iterate (fn [x] (* x 2)) 1))) 
+ ; (iterate) is another way to instantiate a generator; this time with your own fn 
+ 
+ "Repetition is key"
+ (= [:a :a :a :a :a :a :a :a :a :a]
+    (repeat 10 :a)) ; (repeat) is another way to instantiate a generator
 
-  "The range starts at the beginning by default"
-  (= __ (range 5))
+ "Iteration can be used for repetition"
+ (= (repeat 100 "hello")  ; a generator instance, i.e. a lazy sequence
+    (take 100 (iterate (fn [x] x) "hello")))) 
+; (iterate) creates a lazy sequence i.e. a generator
+; but (take) __realizes__ it
+; and then the first lazy sequence (based on (repeat)) is compared to the results of (take)
 
-  "Only take what you need when the sequence is large"
-  (= [0 1 2 3 4 5 6 7 8 9]
-     (take __ (range 100)))
-
-  "Or limit results by dropping what you don't need"
-  (= [95 96 97 98 99]
-     (drop __ (range 100)))
-
-  "Iteration provides an infinite lazy sequence"
-  (= __ (take 8 (iterate (fn [x] (* x 2)) 1)))
-
-  "Repetition is key"
-  (= [:a :a :a :a :a :a :a :a :a :a]
-     (repeat 10 __))
-
-  "Iteration can be used for repetition"
-  (= (repeat 100 "hello")
-     (take 100 (iterate ___ "hello"))))
