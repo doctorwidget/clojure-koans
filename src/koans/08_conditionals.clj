16ce462a9d6e505b661110ca842feba905737858
Scott N Fitz <doctorwidget@gmail.com>
2019-01-06 16:18:41 -0800

    solutions for 08_conditionals

--- src/koans/08_conditionals.clj
+++ src/koans/08_conditionals.clj
@@ -10,38 +10,38 @@
 
 (meditations
   "You will face many decisions"
-  (= __ (if (false? (= 4 5))
+  (= :a (if (false? (= 4 5))  ; if is another fundamental special form
           :a
           :b))
 
   "Some of them leave you no alternative"
-  (= __ (if (> 4 3)
+  (= [] (if (> 4 3) ; you don't have to provide the `else` clause
           []))
 
   "And in such a situation you may have nothing"
-  (= __ (if (nil? 0)
+  (= nil (if (nil? 0)  ; if you do not provide `else`, you get nil by default
           [:a :b :c]))
 
   "In others your alternative may be interesting"
-  (= :glory (if (not (empty? ()))
+  (= :glory (if (not (empty? ())) ; so provide an else or else!
               :doom
-              __))
+              :glory))
 
   "You may have a multitude of possible paths"
-  (let [x 5]
-    (= :your-road (cond (= x __) :road-not-taken
-                        (= x __) :another-road-not-taken
-                        :else __)))
+  (let [x 5]  ; (cond) elegantly prevent the need for nested if-else expressions
+    (= :your-road (cond (= x 1) :road-not-taken
+                        (= x -1) :another-road-not-taken
+                        :else :your-road)))
 
   "Or your fate may be sealed"
-  (= 'doom (if-not (zero? __)
+  (= 'doom (if-not (zero? 1) ; (if-not) is just (if) with the clause order reversed
           'doom
           'more-doom))
 
   "In case of emergency, go fast"
-  (= "pretty fast"
-     (explain-exercise-velocity __))
+  (= "pretty fast"  ; (case) is like cond, but the test is always (= %1 %2)
+     (explain-exercise-velocity :bicycling))
 
   "But admit it when you don't know what to do"
-  (= __
+  (= "is that even exercise?"   ; which frankly seems pretty similar to using (get) on a map, with a third (default) arg
      (explain-exercise-velocity :watching-tv)))
