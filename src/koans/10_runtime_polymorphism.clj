48dfe55c4419257f63f6953fffa8d9d494cffdeb
Scott N Fitz <doctorwidget@gmail.com>
2019-01-06 16:47:27 -0800

    solutions for 10_runtime_polymorphism.clj

--- src/koans/10_runtime_polymorphism.clj
+++ src/koans/10_runtime_polymorphism.clj
@@ -1,7 +1,7 @@
 (ns koans.10-runtime-polymorphism
   (:require [koan-engine.core :refer :all]))
 
-(defn hello
+(defn hello    ; arity-based polymorphism is cheap, easy and simple in Clojure
   ([] "Hello World!")
   ([a] (str "Hello, you silly " a "."))
   ([a & more] (str "Hello to this group: "
@@ -9,34 +9,43 @@
                           (interpose ", " (cons a more)))
                    "!")))
 
-(defmulti diet (fn [x] (:eater x)))
-(defmethod diet :herbivore [a] __)
-(defmethod diet :carnivore [a] __)
-(defmethod diet :default [a] __)
+(defmulti diet (fn [x] (:eater x))) ; multimethods require you to apparantly re-declare the same function
+(defmethod diet :herbivore [a] (str (:name a) " eats veggies."))  ; over and over and
+(defmethod diet :carnivore [a] (str (:name a) " eats animals.")) ; over and over...
+(defmethod diet :default [a] (str "I don't know what " (:name a) " eats.")) ; which is unsettling until you get used to it
+; think of the above declaration chain as building up a big (cond) statement somewhere hidden from view
 
 (meditations
-  "Some functions can be used in different ways - with no arguments"
-  (= __ (hello))
+ "Some functions can be used in different ways - with no arguments"
+ (= "Hello World!" (hello)) ; arity-based polymophism can fill the role of default arguments
+ 
+ "With one argument"
+ (= "Hello, you silly world." (hello "world")) ; but it can also allow different *logic* based on arity
+ 
+ "Or with many arguments"
+ (= "Hello to this group: Peter, Paul, Mary!"
+    (hello "Peter" "Paul" "Mary"))
+ ; so all things considered, arity-based polymorphism is a bigger deal than mere default arguments
+ 
+ "Multimethods allow more complex dispatching"
+ (= "Bambi eats veggies."
+    (diet {:species "deer" :name "Bambi" :age 1 :eater :herbivore}))
+ ; step 1: apply the (defmulti) fn to the original input; that yields :herbivore
+ ; step 2: apply the one-and-only fn associated with :herbivore to the _same_ original input!
+ 
+ "Animals have different names"
+ (= "Thumper eats veggies."
+    (diet {:species "rabbit" :name "Thumper" :age 1 :eater :herbivore}))
+ ; so now any entity with both :eater and :name can be used with this multimethod
+ 
+ "Different methods are used depending on the dispatch function result"
+ (= "Simba eats animals."
+    (diet {:species "lion" :name "Simba" :age 1 :eater :carnivore}))
+ ; step 1: apply the (defmulti) fn to the original input; that yields :carnivore
+ ; step 2: apply the one-and-only fn associated with :carnivore to the _same_ original input!
 
-  "With one argument"
-  (= __ (hello "world"))
-
-  "Or with many arguments"
-  (= __
-     (hello "Peter" "Paul" "Mary"))
-
-  "Multimethods allow more complex dispatching"
-  (= "Bambi eats veggies."
-     (diet {:species "deer" :name "Bambi" :age 1 :eater :herbivore}))
-
-  "Animals have different names"
-  (= "Thumper eats veggies."
-     (diet {:species "rabbit" :name "Thumper" :age 1 :eater :herbivore}))
-
-  "Different methods are used depending on the dispatch function result"
-  (= "Simba eats animals."
-     (diet {:species "lion" :name "Simba" :age 1 :eater :carnivore}))
-
-  "You may use a default method when no others match"
-  (= "I don't know what Rich Hickey eats."
-     (diet {:name "Rich Hickey"})))
+ "You may use a default method when no others match"
+ (= "I don't know what Rich Hickey eats."
+    (diet {:name "Rich Hickey"})))
+; step 1: apply the (defmulti) fn to the original input; that yields nil
+; step 2: no matching fn for nil, so apply the :default fn to the original input
