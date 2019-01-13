3f3c03645c755a526c30acf5b2d0f075b3f0a2f5
Scott N Fitz <doctorwidget@gmail.com>
2019-01-13 11:57:36 -0800

    solutions for 24_macros

--- src/koans/24_macros.clj
+++ src/koans/24_macros.clj
@@ -9,31 +9,31 @@
 
 (defmacro infix-concise [form]
   `(~(second form) ; Note the syntax-quote (`) and unquote (~) characters!
-    __
-    __))
+    ~(first form)
+    ~(nth form 2)))
 
-(defmacro recursive-infix [form]
-  (cond (not (seq? form))
-        __
-        (= 1 (count form))
-        `(recursive-infix ~(first form))
-        :else
-        (let [operator (second form)
-              first-arg (first form)
-              others __]
+(defmacro recursive-infix [form] ; cond expects pairs, which makes this fn is hard to read
+  (cond (not (seq? form)) ; test A - test for the base case... is this a non-empty sequence?
+        form              ; result A - non-sequence or empty sequence, so return it as is
+        (= 1 (count form)) ; test B - test for seq of length 1: there is only one element!
+        `(recursive-infix ~(first form)) ; result B - recurse into that seq element
+        :else              ; all other cases MUST BE sequences of length 2 or more
+        (let [operator (second form)  ; this is the fn - it will go into lisp function position
+              first-arg (first form)  ; this is arg#1 - will go into the second spot
+              others (drop 2 form)]     ; all others need to be unraveled as args 2..N
           `(~operator
             (recursive-infix ~first-arg)
             (recursive-infix ~others)))))
 
 (meditations
   "Macros are like functions created at compile time"
-  (= __ (hello "Macros!"))
+  (= "Hello, Macros!" (hello "Macros!"))
 
   "I can haz infix?"
-  (= __ (infix (9 + 1)))
+  (= 10 (infix (9 + 1)))
 
   "Remember, these are nothing but code transformations"
-  (= __ (macroexpand '(infix (9 + 1))))
+  (= '(+ 9 1) (macroexpand '(infix (9 + 1))))
 
   "You can do better than that - hand crafting FTW!"
   (= '(* 10 2) (macroexpand '(infix-concise (10 * 2))))
